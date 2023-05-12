3917f4d5542721ac105ba9cd688f2c3211fe6389
Tyler Garner <tyler@garnercode.io>
2023-05-11 21:29:37 -0500

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <main>
+            <Nuxt />
+        </main>
+    </div>
+</template>
+
+<style lang="scss">
+@import 'static/assets/styles.scss';
+
+    :root {
+        --color-primary: #1B1D23;
+        --color-dark-gray: #60636D;
+        --color-med-gray: #7D828F;
+        --color-light-gray: #C8CCD8;
+        --color-lighter-gray: #EEEFF4;
+        --color-red: #DF5656;
+
+        --transition: 0.3s ease;
+    }
+    body {
+        font-family: 'League Spartan', sans-serif;
+        margin: 0;
+    }
+    h1, h2, h3, h4 {
+        font-weight: bold;
+        margin: 0;
+    }
+    h2, h3, h4 {
+        color: var(--color-primary);
+    }
+    p {
+        color: var(--color-dark-gray);
+        font-size: 18px;
+        font-weight: 500;
+    }
+    .button {
+        border: none;
+        font-size: 18px;
+        font-weight: bold;
+        padding: 2rem;
+        cursor: pointer;
+        transition: var(--transition);
+        &.button-default {
+            background-color: var(--color-primary);
+            color: white;
+            &:hover {
+                background-color: var(--color-dark-gray);
+            }
+        }
+    }
+    @media screen and (min-width: 0px) {
+        h1 {
+            display: none;
+        }
+    }
+    @media screen and (min-width: 768px) {
+        h1 {
+            display: block;
+            font-size: 120px;
+            color: var(--color-lighter-gray);
+        }
+    }
+    @media screen and (min-width: 1440px) {
+        h1 {
+            font-size: 250px;
+        }
+        h2 {
+            font-size: 96px;
+        }
+        h3 {
+            font-size: 72px;
+        }
+        h4 {
+            font-size: 32px;
+        }
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Test Header</h1>
+    <h2>Test Header 2</h2>
+    <h3>Test Header 3</h3>
+    <h4>Test Header 4</h4>
+    <p>This is a test message.</p>
+    <button class="button button-default">Button 1</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
