58e0a620a115f2e8a73b0821d39b18efe4fc52be
Tyler Garner <tyler@garnercode.io>
2023-05-16 16:25:14 -0500

    Completed Portfolio page

--- const/portfolioData.ts
+++ const/portfolioData.ts
@@ -3,61 +3,121 @@ export const portfolioData = [
         title: 'Seraph Station',
         date: 'September 2019',
         description: 'The Seraph Station project challenged us to design a unique station that would transport people through time. The result is a fresh and futuristic model inspired by space stations.',
+        image: {
+            mobile: 'assets/portfolio/mobile/image-seraph.jpg',
+            tablet: 'assets/portfolio/tablet/image-seraph.jpg',
+            desktop: 'assets/portfolio/desktop/image-seraph.jpg',
+        }
     },
     {
         title: 'Eebox Building',
         date: 'August 2017',
         description: '',
+        image: {
+            mobile: 'assets/portfolio/mobile/image-eebox.jpg',
+            tablet: 'assets/portfolio/tablet/image-eebox.jpg',
+            desktop: 'assets/portfolio/desktop/image-eebox.jpg',
+        }
     },
     {
         title: 'Federal II Tower',
         date: 'March 2017',
         description: 'A sequel theme project for a tower originally built in the 1800s. We achieved this with a striking look of brutal minimalism with modern touches.',
+        image: {
+            mobile: 'assets/portfolio/mobile/image-federal.jpg',
+            tablet: 'assets/portfolio/tablet/image-federal.jpg',
+            desktop: 'assets/portfolio/desktop/image-federal.jpg',
+        }
     },
     {
         title: 'Project Del Sol',
         date: 'January 2016',
         description: '',
+        image: {
+            mobile: 'assets/portfolio/mobile/image-del-sol.jpg',
+            tablet: 'assets/portfolio/tablet/image-del-sol.jpg',
+            desktop: 'assets/portfolio/desktop/image-del-sol.jpg',
+        }
     },
     {
         title: 'Le Prototype',
         date: 'October 2015',
         description: '',
+        image: {
+            mobile: 'assets/portfolio/mobile/image-prototype.jpg',
+            tablet: 'assets/portfolio/tablet/image-prototype.jpg',
+            desktop: 'assets/portfolio/desktop/image-prototype.jpg',
+        }
     },
     {
         title: '228B Tower',
         date: 'April 2015',
         description: '',
+        image: {
+            mobile: 'assets/portfolio/mobile/image-228b.jpg',
+            tablet: 'assets/portfolio/tablet/image-228b.jpg',
+            desktop: 'assets/portfolio/desktop/image-228b.jpg',
+        }
     },
     {
         title: 'Grand Edelweiss Hotel',
         date: 'December 2013',
         description: '',
+        image: {
+            mobile: 'assets/portfolio/mobile/image-edelweiss.jpg',
+            tablet: 'assets/portfolio/tablet/image-edelweiss.jpg',
+            desktop: 'assets/portfolio/desktop/image-edelweiss.jpg',
+        }
     },
     {
         title: 'Netcry Tower',
         date: 'August 2012',
         description: '',
+        image: {
+            mobile: 'assets/portfolio/mobile/image-netcry.jpg',
+            tablet: 'assets/portfolio/tablet/image-netcry.jpg',
+            desktop: 'assets/portfolio/desktop/image-netcry.jpg',
+        }
     },
     {
         title: 'Hypers',
         date: 'January 2012',
         description: '',
+        image: {
+            mobile: 'assets/portfolio/mobile/image-hypers.jpg',
+            tablet: 'assets/portfolio/tablet/image-hypers.jpg',
+            desktop: 'assets/portfolio/desktop/image-hypers.jpg',
+        }
     },
     {
         title: 'SXIV Tower',
         date: 'March 2011',
         description: '',
+        image: {
+            mobile: 'assets/portfolio/mobile/image-sxiv.jpg',
+            tablet: 'assets/portfolio/tablet/image-sxiv.jpg',
+            desktop: 'assets/portfolio/desktop/image-sxiv.jpg',
+        }
     },
     {
         title: 'Trinity Bank Tower',
         date: 'September 2010',
         description: 'Trinity Bank challenged us to make a concept for a 84 story building located in the middle of a city with a high earthquake frequency. For this project we used curves to blend design and stability to meet our objectives.',
+        image: {
+            mobile: 'assets/portfolio/mobile/image-trinity.jpg',
+            tablet: 'assets/portfolio/tablet/image-trinity.jpg',
+            desktop: 'assets/portfolio/desktop/image-trinity.jpg',
+        }
     },
     {
         title: 'Project Paramour',
         date: 'Febuary 2008',
         description: 'Project made for an art museum near Southwest London. Project Paramour is a statement of bold, modern architecture.',
+        image: {
+            mobile: 'assets/portfolio/mobile/image-paramour.jpg',
+            tablet: 'assets/portfolio/tablet/image-paramour.jpg',
+            desktop: 'assets/portfolio/desktop/image-paramour.jpg',
+        }
     },
 ]
 
---
+++ pages/portfolio.vue
@@ -0,0 +1,110 @@
+<template>
+    <div class="portfolio-page">
+        <div class="projects-container">
+            <div class="project" v-for="(project, index) of portfolioData" :key="index">
+                <img class="project-img img-mobile" :src="project.image.mobile" alt="">
+                <img class="project-img img-tablet" :src="project.image.tablet" alt="">
+                <img class="project-img img-desktop" :src="project.image.desktop" alt="">
+                <div class="project-text">
+                    <div class="project-title">
+                        {{ project.title }}
+                    </div>
+                    <div class="project-date">
+                        {{ project.date }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+    @media screen and (min-width: 0px) {
+        .portfolio-page {
+            padding: 0 var(--mobile-x-padding);
+            .projects-container {
+                display: flex;
+                flex-direction: column;
+                gap: 3rem;
+                .project {
+                    position: relative;
+                }
+                .project-img {
+                    width: 100%;
+                    filter: var(--image-filter);
+                }
+                .img-tablet {
+                    display: none;
+                }
+                .img-desktop {
+                    display: none;
+                }
+                .project-text {
+                    position: absolute;
+                    bottom: 4rem;
+                    left: 3rem;
+                    display: flex;
+                    flex-direction: column;
+                    gap: 1rem;
+                }
+                .project-title {
+                    font-size: 32px;
+                    color: white;
+                    font-weight: bold;
+                }
+                .project-date {
+                    font-size: 18px;
+                    color: white;
+                    opacity: 0.75;
+                    font-weight: medium;
+                }
+            }
+        }
+    }
+    @media screen and (min-width: 768px) {
+        .portfolio-page {
+            padding: 0 var(--tablet-x-padding);
+            .projects-container {
+                .project-img {
+                    width: 100%;
+                }
+                .img-mobile {
+                    display: none;
+                }
+                .img-tablet {
+                    display: block;
+                }
+            }
+        }
+    }
+    @media screen and (min-width: 1440px) {
+        .portfolio-page {
+            padding: 0 var(--desktop-x-padding);
+            .projects-container {
+                display: grid;
+                grid-template-columns: 1fr 1fr 1fr;
+                gap: 3rem;
+                .img-tablet {
+                    display: none;
+                }
+                .img-desktop {
+                    display: block;
+                }
+            }
+        }
+    }
+</style>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    import { portfolioData } from '~/const/portfolioData';
+
+    export default defineComponent({
+        name: 'Portfolio',
+        data: () => {
+            return {
+                portfolioData,
+            }
+        }
+    })
+</script>
