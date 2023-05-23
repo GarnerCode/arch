88c9ccae46db2551ac85c83ce5ce4085f60a1403
Tyler Garner <tyler@garnercode.io>
2023-05-23 12:21:03 -0500

    Setting up About page

---
+++ const/leaderData.ts
@@ -0,0 +1,22 @@
+export const leaderData = [
+    {
+        imageUrl: 'assets/about/desktop/avatar-jake.jpg',
+        name: 'Jake Richards',
+        title: 'Chief Architect',
+    },
+    {
+        imageUrl: 'assets/about/desktop/avatar-thompson.jpg',
+        name: 'Thompson Smith',
+        title: 'Head of Finance',
+    },
+    {
+        imageUrl: 'assets/about/desktop/avatar-jackson.jpg',
+        name: 'Jackson Rourke',
+        title: 'Lead Designer',
+    },
+    {
+        imageUrl: 'assets/about/desktop/avatar-maria.jpg',
+        name: 'Maria Simpson',
+        title: 'Senior Architect',
+    },
+]
--- pages/about.vue
+++ pages/about.vue
@@ -8,7 +8,7 @@
                 <img src="assets/about/desktop/image-hero.jpg" alt="">
             </picture>
             <div class="about-text">
-                <div class="text-container">
+                <div class="text-container text-overlay">
                     <h2>Your team of professionals</h2>
                     <p>Our small team of world-class professionals will work with you every step of the way. Strong relationships are at the core of everything we do. This extends to the relationship our projects have with their surroundings.</p>
                 </div>
@@ -20,9 +20,15 @@
                 </div>
             </div>
             <div class="about-leaders">
-                <h2>The leaders</h2>
-                <div class="leader">
-
+                <h2>The<br/> Leaders</h2>
+                <div class="leaders-container">
+                    <div class="leader" v-for="(leader, index) of leaderData" :key="index">
+                        <img class="leader-img" :src="leader.imageUrl" :alt="`${leader.name} headshot`">
+                        <div class="leader-text">
+                            <h4 class="leader-name">{{ leader.name }}</h4>
+                            <p class="leader-title">{{ leader.title }}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -31,7 +37,53 @@
 
 <style lang="scss">
     @media screen and (min-width: 0px) {
-
+        .about-page {
+            .about-content {
+                position: relative;
+                display: flex;
+                flex-direction: column;
+                gap: 5rem;
+            }
+            .about-text {
+                padding: 0 var(--mobile-x-padding);
+            }
+            .text-overlay {
+                position: relative;
+                bottom: 12rem;
+                right: 5rem;
+                background-color: white;
+                padding: 9rem 5rem;
+                padding-right: 0;
+                width: 100%;
+            }
+            .about-leaders {
+                padding: 0 var(--mobile-x-padding);
+                display: flex;
+                flex-direction: column;
+                gap: 9rem;
+            }
+            .leaders-container {
+                display: flex;
+                flex-direction: column;
+                gap: 9rem;
+            }
+            .leader {
+                display: flex;
+                flex-direction: column;
+                gap: 3rem;
+            }
+            .leader-img {
+                width: 100%;
+            }
+            .leader-text {
+                display: flex;
+                flex-direction: column;
+                gap: 0.5rem;
+            }
+            .leader-title {
+                margin: 0;
+            }
+        }
     }
     @media screen and (min-width: 768px) {
 
@@ -43,12 +95,13 @@
 
 <script lang="ts">
     import { defineComponent } from 'vue';
+    import { leaderData } from '@/const/leaderData';
 
     export default defineComponent({
         name: 'AboutPage',
         data: () => {
             return {
-
+                leaderData,
             }
         }
     })
